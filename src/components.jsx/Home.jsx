fcbe87a6cddae962a287a482577a27cb8064a78f
HeatherBerrill <heather.berrill@btinternet.com>
2021-10-04 15:56:26 +0100

    styling home page and media queries for desktop

--- src/components.jsx/Home.jsx
+++ src/components.jsx/Home.jsx
@@ -5,6 +5,7 @@ import '../Styles/index.css';
 import Footer from './Footer';
 import { getVotedReviews } from '../api';
 import { Button, CircularProgress } from '@mui/material';
+import background from '../Images/outline2.jpg';
 
 const Home = ({ isLoading, setIsLoading, setReviews, reviews }) => {
   useEffect(() => {
@@ -38,49 +39,21 @@ const Home = ({ isLoading, setIsLoading, setReviews, reviews }) => {
           <p className='menubar__item menubar__users'> our users</p>
         </Link>
       </div>
-
-      <div className='home__reviews-list-container'>
-        <p className='rated-reviews__title'> most voted reviews</p>
-        <ul className='reviews__list'>
-          {reviews.map((review) => {
-            return (
-              <li className={'home__single-review'} key={review.review_id}>
-                <h3 className='home__review-category'> {review.category}</h3>
-                <Link to={`/reviews/${review.review_id}`} className='btn_link'>
-                  <Button
-                    variant='contained'
-                    color='primary'
-                    size='small'
-                    //  className='btn home__review-btn'
-                  >
-                    Read Review
-                  </Button>
-                </Link>
-                <h3 className='home__review-title'> {review.title} </h3>
-
-                <p className='home__review-owner'> {review.owner} </p>
-                <img
-                  alt='review'
-                  className='home__review-list-thumbnail'
-                  src={review.review_img_url}
-                ></img>
-                <p className='home__review-votes'> votes: {review.votes} </p>
-              </li>
-            );
-          })}
-        </ul>
+      <div className='home-content'>
+        <p className='games-welcome__1'>Welcome to NC Games!</p>
+        <p className='games-welcome__2'>Review your favourite games</p>
         <Link to={'/reviews'} className='btn_link'>
           <Button
             variant='contained'
             color='primary'
             size='small'
-            // className='btn home__review-btn'
+            className='home__review-btn'
+            style={{ margin: 45 }}
           >
-            All Reviews
+            Get Started...
           </Button>
         </Link>
       </div>
-      <Footer className='home__footer' />
     </section>
   );
 };
